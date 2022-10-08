d7815844d43f0fe4afaa245d92b3f7cdca9fd1cb
Programmerdin <testingemailodin@gmail.com>
2022-10-09 01:23:55 +0900

    added History page, currently empty

---
+++ Screens/HIstoryScreen.js
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import HistoryComponent from "../components/HistoryComponent";
+
+
+export default function HistoryScreen({navigation}){
+  
+  return(
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <HistoryComponent navigation={navigation}/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1
+  },
+  HistoryComponent:{
+    flex:1,
+  },
+})
--- components/MainPageComponent.js
+++ components/MainPageComponent.js
@@ -1,43 +1,38 @@
 import { StatusBar } from "expo-status-bar";
 import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 
-
-
-export default function MainPageComponent({navigation, route}){
-
-  return(
+export default function MainPageComponent({ navigation, route }) {
+  return (
     <View style={styles.the_View}>
       <StatusBar style="auto" />
-      
+
       {/* TouchableOpacity that navigates to DivisionQuizScreen */}
-      <TouchableOpacity
-        style={styles.navigation_button}
-        onPress={() => navigation.navigate("DivisionQuiz")}
-      >
+      <TouchableOpacity style={styles.navigation_button} onPress={() => navigation.navigate("DivisionQuiz")}>
         <Text style={styles.regular_text}>Division</Text>
       </TouchableOpacity>
 
       {/* TouchableOpacity that navigates to CompoundingQuizScreen */}
-      <TouchableOpacity
-        style={styles.navigation_button}
-        onPress={() => navigation.navigate("CompoundingQuiz")}
-      >
+      <TouchableOpacity style={styles.navigation_button} onPress={() => navigation.navigate("CompoundingQuiz")}>
         <Text style={styles.regular_text}>Compounding</Text>
       </TouchableOpacity>
-      
+
+      {/* TouchableOpacity that navigates to HistoryScreen */}
+      <TouchableOpacity style={styles.navigation_button} onPress={() => navigation.navigate("History")}>
+        <Text style={styles.regular_text}>History</Text>
+      </TouchableOpacity>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  the_View:{
-    justifyContent: 'center', //Centered vertically
-    alignItems: 'center', // Centered horizontally
-    flex:1,
-    backgroundColor:'#121212',
-    color: 'white',
+  the_View: {
+    justifyContent: "center", //Centered vertically
+    alignItems: "center", // Centered horizontally
+    flex: 1,
+    backgroundColor: "#121212",
+    color: "white",
   },
-  navigation_button:{
+  navigation_button: {
     width: 190,
     height: 70,
     backgroundColor: "#BB86FC",
@@ -46,10 +41,10 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     margin: 10,
   },
-  regular_text:{
-    color: '#121212',
-    fontWeight: 'bold',
-    textAlign: 'center',
+  regular_text: {
+    color: "#121212",
+    fontWeight: "bold",
+    textAlign: "center",
     fontSize: 22,
-  }
-})
+  },
+});
